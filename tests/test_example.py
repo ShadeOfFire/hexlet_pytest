540f339f1a5232db60a633ef632ca978b68aad2c
airmage <airmage2@gmail.com>
2021-09-25 03:09:01 +0300

    Added tests for stack

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,25 @@
 from hexlet_pytest.example import reverse
+import pytest
+
 
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+def test_stack():
+    stack = []
+# Добавляем два элемента в стек и затем извлекаем их
+# Почему два? Так надежнее, чем один, а три скорее всего избыточно
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
